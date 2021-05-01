b0c7fa048b2df334ba139b18c0e86b7469d415d9
R2D2Alfa <arturo.ad2203@comunidad.unam.mx>
2021-05-01 00:02:27 -0500

    FEAT: Agregar comentarios

--- Actividad 9/Actividad 9.php
+++ Actividad 9/Actividad 9.php
@@ -9,15 +9,19 @@
 <?php  
     $t=6;
     $x=0;
+    $y=0;
     echo"<table border=1>";
         echo"<tbody>";
-            for ($x=0; $x<$t ; $x++) { 
-                for ($y=0; $y<$t ; $y++) { 
+            while ($x<$t) { 
+                //Siempre y cuando x sea menor que t se va a imprimir una fila con imagen blanca
+                while ($y<$t) { 
                    if ($x%2==0) {
                        if ($y%2==0) {
+                           // Si el % de "y" y de "x" tienen el mismo valor (0 o 1) se imprimirá una imagen blanca
                              echo "<img src='./blanco (1).jpg' alt=Imagen de recuadro blanco height='100' width='100'>";
                          }
                          else
+                          // Si el % de "y" y de "x" tienen diferente valor (0 y 1) se imprimirá una imagen negra
                          echo "<img src='./negro (1).jpg' alt=Imagen de recuadro negro height='100' width='100'>";
                    }
                    else{
@@ -27,8 +31,10 @@
                          else
                             echo "<img src='./blanco (1).jpg' alt=Imagen de recuadro blanco height='100' width='100'>"; 
                      }
-
+                     $y=$y+1;
                 }
+                $y=0;
+                $x=$x+1;
                 echo"<br>";
             }
         echo"</tbody>";
